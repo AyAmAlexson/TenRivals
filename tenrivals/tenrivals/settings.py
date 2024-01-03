ff56a844cb60b14fad7a94703003b09b790928e8
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2024-01-03 22:17:12 +0400

    initial heroku launch - bug fixes

--- tenrivals/tenrivals/settings.py
+++ tenrivals/tenrivals/settings.py
@@ -116,9 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
