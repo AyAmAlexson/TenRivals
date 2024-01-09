e14c9c24de1e3ed4b928389d47079fc932e32acc
AyAmAlexson <125996865+AyAmAlexson@users.noreply.github.com>
2024-01-09 21:20:25 +0400

    rivals init

--- tenrivals/tenrivals/settings.py
+++ tenrivals/tenrivals/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.flatpages',
 
     'persons',
+    'rivals'
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
--- tenrivals/tenrivals/urls.py
+++ tenrivals/tenrivals/urls.py
@@ -5,7 +5,7 @@ from persons.views import CustomLoginView, CustomSignupView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('persons.urls')),
-    # path('rivals/', include('rivals.urls')),
+    path('rivals/', include('rivals.urls')),
     path('accounts/login/', CustomLoginView.as_view(), name='account_login'),
     path('accounts/signup/', CustomSignupView.as_view(), name='account_signup'),
     path('accounts/', include('allauth.urls')),
