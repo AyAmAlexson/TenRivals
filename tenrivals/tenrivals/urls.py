04a37d0a4f86ab9250744a7958b0e4fa27f0c4bb
AyAmAlexson <a.molodenko@gmail.com>
2026-04-02 19:23:31 +0400

    login form added

--- tenrivals/tenrivals/urls.py
+++ tenrivals/tenrivals/urls.py
@@ -11,6 +11,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(pattern_name='shop:index', permanent=False)),
     path('shop/', include('shop.urls')),
+    path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
